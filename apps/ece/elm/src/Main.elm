d45493258e5a722ac0924a93718ae619386381e4
Ralf Th. Pietsch <ratopi@abwesend.de>
2019-05-04 19:31:37 +0200

    now can read counter initially

--- apps/ece/elm/src/Main.elm
+++ apps/ece/elm/src/Main.elm
@@ -1,25 +1,88 @@
 import Browser
-import Html exposing (button, div, node, text)
+import Html exposing (Html, button, div, node, text)
 import Html.Events exposing (onClick)
 import Html.Attributes exposing (class)
+import Http
+import Json.Decode as JD
 
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
 
-type Msg = Increment | Decrement
+type Counter
+  = Int
 
 
+type Msg
+    = Get
+    | GotCounter (Result Http.Error Int)
+    | Increment
+    | Decrement
+    | GotText (Result Http.Error String)
+
+type Model
+  = Failure
+  | Loading
+  | Success String
+  | Counter Int
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    ( Loading, get_counter )
+
+xinit _ =
+  ( Loading
+  , Http.get
+      { url = "counter"
+      , expect = Http.expectString GotText
+      }
+  )
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    Get ->
+        (model, Cmd.none)
+
+    GotCounter result ->
+        case result of
+            Ok n ->
+                (Counter n, Cmd.none)
+            Err _ ->
+                (Failure, Cmd.none)
+
+
     Increment ->
-      model + 1
+        increment model
 
     Decrement ->
-      model - 1
+      decrement model
+
+    GotText result ->
+      case result of
+        Ok fullText ->
+            (Counter 1000, Cmd.none)
+--          (Success fullText, Cmd.none)
+
+        Err _ ->
+          (Failure, Cmd.none)
 
 
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
 view model =
     div [] [
         nav_bar,
@@ -47,8 +110,48 @@ counter_card model =
 
 
 counter model =
+    case model of
+        Failure -> text "Error"
+        Loading -> text "Loading..."
+        Success _ -> text "Success"
+        Counter n -> counter_and_buttons n
+
+
+counter_and_buttons : Int -> Html Msg
+counter_and_buttons n =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text (String.fromInt n) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
+
+
+increment : Model -> (Model, Cmd Msg)
+increment model =
+    case model of
+        Failure -> (Failure, Cmd.none)
+        Loading -> (Loading, Cmd.none)
+        Success _ -> (Counter 0, Cmd.none)
+        Counter n -> (Counter (n + 1), Cmd.none)
+
+
+decrement : Model -> (Model, Cmd Msg)
+decrement model =
+    case model of
+        Failure -> (Failure, Cmd.none)
+        Loading -> (Loading, Cmd.none)
+        Success _ -> (Counter 0, Cmd.none)
+        Counter n -> (Counter (n - 1), Cmd.none)
+
+
+
+get_counter : Cmd Msg
+get_counter =
+  Http.get
+      { url = "counter"
+      , expect = Http.expectJson GotCounter counter_json_decode
+      }
+
+counter_json_decode : JD.Decoder Int
+counter_json_decode =
+    JD.field "counter" JD.int
